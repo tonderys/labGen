718071d0d0b343dd41153308c3a98a0fb2032984
Szymon <tonszym@gmail.com>
2014-04-15 06:11:56 +0200

    Create Point.cs
    First basic struct file

---
+++ Point.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+namespace labyrinthGenerator
+{
+	public class Point {
+		public int width, height; 
+		
+		public Point(int x, int y) {
+			width = x;
+			height = y;
+		}
+	}
+}
